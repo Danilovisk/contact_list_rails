0d93eefa53c6005462f2c56a6b902938d7d8a498
Danilo <danilo05.mesquita@gmail.com>
2021-10-04 21:23:08 -0300

    Relação entre Usuario e Contatos

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
-    protect_from_forgery with: :exception
-    add_flash_types :danger, :info ,:warning, :success 
-    include SessionsHelper
+  protect_from_forgery with: :exception
+  add_flash_types :danger, :info ,:warning, :success 
+  include SessionsHelper    
+  
+  private
+    def required_logged_in_user
+      unless user_signed_in?
+        flash[:danger] = "Area restrita. Por favor, realize o login."
+        redirect_to entrar_path
+      end
+    end
 end
